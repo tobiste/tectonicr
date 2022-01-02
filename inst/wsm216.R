984a92ed6745e5a06f19f8c8187cfa677e290a72
T. Stephan <tobiste@googlemail.com>
2022-01-01 17:29:27 -0700

    upload example datasets

---
+++ inst/wsm216.R
@@ -0,0 +1,23 @@
+#' World Stress Map
+#'
+#' The World Stress Map (WSM) is a global compilation of information on the
+#' crustal present-day stress field. Version 2016
+#'
+#' @docType data
+#'
+#' @usage data(wsm2016)
+#'
+#' @format An object of class \code{"data.frame"}
+#'
+#' @references Heidbach, O., M. Rajabi, X. Cui, K. Fuchs, B. Müller, J.
+#' Reinecker, K. Reiter, M. Tingay, F. Wenzel, F. Xie, M. O. Ziegler,
+#' M.-L. Zoback, and M. D. Zoback (2018): The World Stress Map database
+#' release 2016: Crustal stress pattern across scales. Tectonophysics, 744,
+#' 484-498, doi:10.1016/j.tecto.2018.07.007
+#' @keywords datasets
+#' @examples
+#'
+#' data(wsm216)
+#' head(wsm216)
+#'
+"wsm216"
