8a397c76f44f150add0dabfe0657f5a221811c4a
Tobias Stephan <tobias.stephan@ucalgary.ca>
2022-01-04 19:29:32 -0700

    adds Euler pole spatial objects

---
+++ R/draw_eulerpole.R
@@ -0,0 +1,257 @@
+smallcircle_dummy <- function(gridsize) {
+  sm_range <- seq(0, 180, gridsize)
+  lons <- seq(-180, 180, gridsize)
+
+  sm.df <- data.frame(
+    "lon" = as.numeric(),
+    "lat" = as.numeric(),
+    "small_circle" = as.numeric()
+  )
+
+  for (i in sm_range) {
+    # loop through all small circles
+    sm <- sm_range[sm_range == i]
+    lat <- sm - 90
+    sm.l <- data.frame(
+      "lat" = rep(lat, length(lons)),
+      "lon" = lons,
+      "small_circle" = sm
+    )
+    sm.df <- rbind(sm.df, sm.l)
+  }
+  return(sm.df)
+}
+
+#' @title Wrap Spatial object at dateline
+#'
+#' @description Wrap a Spatial object at date line
+#'
+#' @param x Spatial object
+#' @return Spatial object
+#' @export
+wrap_dateline <- function(x){
+  x.wrapped.sf <- sf::st_wrap_dateline(
+    sf::st_as_sf(x),
+    options =  c("WRAPDATELINE=YES", "DATELINEOFFSET=180"),
+    quiet = TRUE
+  )
+  x.wrapped <- sf::as_Spatial(x.wrapped.sf)
+
+  return(x.wrapped)
+}
+
+#' @title Draws small-circles around Euler pole
+#'
+#' @description Calculate the direction of maximum horizontal stress along great circles,
+#' small circles, and loxodromes around the relative plate motion´s Euler pole
+#' at a given point or points
+#'
+#' @author Tobias Stephan
+#' @param x \code{data.frame} containing coordinates of Euler pole in lat, lon (and rotation angle [optional])
+#' @param gridsize numeric, angular distance between small circles; default: 10
+#' @return An object of class \code{SpatialLinesDataFrame}
+#' @details if angle is given: output additionally gives absolute velocity on small-circle (degree/Myr -> km/Myr)
+#' @importFrom dplyr "%>%"
+#' @export
+#' @examples
+#' data("nuvel1")
+#' euler <- subset(nuvel1, nuvel1$ID == "na") # North America relative to
+#' euler$angle <- euler$rate
+#' # Pacific plate
+#' eulerpole_smallcircles(euler)
+eulerpole_smallcircles <- function(x, gridsize = 10) {
+  sm.df <- smallcircle_dummy(gridsize)
+  sm_range <- unique(sm.df$small_circle)
+
+  if(is.null(x$angle)){
+    sm_range.df <- sm_range
+  } else {
+    velocity <- sm.df %>%
+      dplyr::mutate(abs_vel = abs_vel(x$angle, sm.df$small_circle)) %>%
+      dplyr::select(small_circle, abs_vel) %>% unique()
+    sm_range.df <- velocity
+  }
+
+  SL.list <- list()
+
+  for (s in unique(sm.df$small_circle)) {
+    # loop through all small circles
+    sm.subset <- subset(sm.df, sm.df$small_circle == s)
+    sm.subset.rot <- sm.subset
+
+    for (i in seq_along(sm.subset$lat)) {
+      #loop through all coordinates
+
+      pt <- c(sm.subset$lat[i], sm.subset$lon[i])
+
+      # Rotation matrix: axis is axis perpendicular to north pole 0/90 and Euler
+      # pole, rotation-angle is angle between northpole and euler pole
+      rot.axis <- pracma::cross(geographical_to_cartesian(c(90, 0)),
+                                geographical_to_cartesian(c(x$lat, x$lon)))
+      rot.angle <-
+        angle_vectors(geographical_to_cartesian(c(90, 0)),
+                      geographical_to_cartesian(c(x$lat, x$lon)))
+      if (is.nan(rot.angle)) {
+        rot.angle <- 0
+      } # if there is no angle between,
+      # than angle=0
+
+      rot.matrix <- rotation_matrix(rot.axis, rot.angle)
+
+      pt1 <- cartesian_to_geographical(rot.matrix %*% geographical_to_cartesian(pt))
+
+      sm.subset.rot$lat[i] <- pt1[1]
+      sm.subset.rot$lon[i] <- pt1[2]
+    }
+
+    l.i <- sp::Lines(slinelist = sp::Line(cbind(
+      "lon" = sm.subset.rot$lon,
+      "lat" = sm.subset.rot$lat
+    )), ID = as.character(s))
+    SL.list[as.character(s)] <- l.i
+
+    if (s == 0) {
+      sm.rot <- sm.subset.rot
+    } else {
+      sm.rot <- rbind(sm.rot, sm.subset.rot)
+    }
+  }
+
+  SL.t <- sp::SpatialLinesDataFrame(
+    sp::SpatialLines(SL.list, proj4string = sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")),
+    data.frame("small_circle" = sm_range.df,
+               row.names = sm_range)
+  )
+
+  SL.t <- wrap_dateline(SL.t)
+
+
+  # if(!is.null(x$angle)){
+  #   SL.t@data$velocity.abs <- pracma::deg2rad(x$angle) * 6371.00887714 * pracma::sind(SL.t@data$small_circle)
+  # }
+  return(SL.t)
+}
+
+greatcircle_dummy <- function(d) {
+  angle <- 360/d
+  #p <- c(0, 0)
+  i <- 0
+  while(i <= 360){
+    if(i%%180 == 0){
+      great.circle.i <- data.frame(
+        lon = rep(0, 181),
+        lat = seq(-90, 90, 1),
+        great.circle = i
+      )
+    } else {
+      great.circle.i <-
+        geosphere::greatCircleBearing(c(0, 0), i) %>%
+        as.data.frame() %>%
+        dplyr::mutate(great.circle = i)
+    }
+
+    if(i == 0){
+      great.circle <- great.circle.i
+    } else {
+      great.circle <- rbind(great.circle, great.circle.i)
+    }
+    i <- i + angle
+  }
+  return(great.circle)
+}
+
+#' @title Draws great-circles of Euler pole
+#'
+#' @description Calculate the direction of maximum horizontal stress along great circles,
+#' small circles, and loxodromes around the relative plate motion´s Euler pole
+#' at a given point or points
+#'
+#' @author Tobias Stephan
+#' @param x \code{data.frame} containing coordinates of Euler poles in lat, lon (and rotaion angle [optional])
+#' @param gridsize numeric, angle between great circles
+#' @param d numeric; number of equally spaced great circles (angle between great circles (number of great circles = 360 / angle); default = 12
+#' @return An object of class \code{SpatialLinesDataFrame}
+#' @details if angle is given: output additionally gives absolute velocity on small-circle (degree/Myr -> km/Myr)
+#' @importFrom dplyr "%>%"
+#' @export
+#' @examples
+#' data("nuvel1")
+#' euler <- subset(nuvel1, nuvel1$ID == "na") # North America relative to
+#' # Pacific plate
+#' euler$angle <- euler$rate
+#' eulerpole_greatcircles(euler)
+eulerpole_greatcircles <- function(x, d, gridsize) {
+  if (missing(d) & !missing(gridsize)) {
+    d = round(360/gridsize, 0)
+  } else if (missing(d) & missing(gridsize)) {
+    d = 12
+  } else if (!missing(d) & !missing(gridsize)) {
+    warning('Both gridzise and d are given. Only d is considered')
+    d = 12
+  }
+
+  gm.df <- greatcircle_dummy(d)
+  gm_range <- unique(gm.df$great.circle)
+  SL.list <- list()
+
+  for (g in unique(gm.df$great.circle)) {
+    # loop through all great circles
+    gm.subset <- subset(gm.df, gm.df$great.circle == g)
+    gm.subset.rot <- gm.subset
+
+    for (i in seq_along(gm.subset$lat)) {
+      #loop through all coordinates
+      pt <- c(gm.subset$lat[i], gm.subset$lon[i])
+
+      # Rotation matrix: axis is axis perpendicular to gm origin pole 0/0 and Euler
+      # pole, rotation-angle is angle between gm origin and euler pole
+      rot.axis <- pracma::cross(geographical_to_cartesian(c(0, 0)),
+                                geographical_to_cartesian(c(x$lat, x$lon)))
+      rot.angle <-
+        angle_vectors(geographical_to_cartesian(c(0, 0)),
+                      geographical_to_cartesian(c(x$lat, x$lon)))
+      if (is.nan(rot.angle)) {
+        rot.angle <- 0
+      } # if there is no angle between,
+      # than angle=0
+
+      rot.matrix <- rotation_matrix(rot.axis, rot.angle)
+
+      pt1 <- cartesian_to_geographical(rot.matrix %*% geographical_to_cartesian(pt))
+
+      gm.subset.rot$lat[i] <- pt1[1]
+      gm.subset.rot$lon[i] <- pt1[2]
+    }
+
+    l.i <- suppressWarnings(
+      sp::Lines(
+        slinelist = sp::Line(cbind(
+          "lon" = gm.subset.rot$lon,
+          "lat" = gm.subset.rot$lat
+        )
+        ), ID = as.character(g)
+      )
+    )
+
+    SL.list[as.character(g)] <- l.i
+
+    if (g == dplyr::first(unique(gm.df$great.circle))) {
+      gm.rot <- gm.subset.rot
+    } else {
+      gm.rot <- rbind(gm.rot, gm.subset.rot)
+    }
+  }
+
+  SL.t <- sp::SpatialLines(SL.list, proj4string = sp::CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+
+  SL.t.df <- suppressWarnings(
+    sp::SpatialLinesDataFrame(
+      SL.t,
+      data.frame("great_circle" = as.character(gm_range), row.names = gm_range)
+    )
+  )
+
+  SL.t.df <- wrap_dateline(SL.t.df)
+
+  return(SL.t.df)
+}
---
+++ R/rotation.R
@@ -0,0 +1,119 @@
+#' @title Angle between two vectors
+#'
+#' @description Calculates the angle between two vectors
+#' @author Tobias Stephan
+#' @param x Vector 1 in cartesian coordinates. Can be a vector of three numbers
+#'  or a matrix of 3 columns (first one is x, second is y, thrid one is z)
+#' @param y Vector 2. As above
+#' @return numeric; angle in degrees
+#' @export
+#' @examples
+#' u <- c(1, -2, 3)
+#' v <- c(-2, 1, 1)
+#' angle_vectors(u, v)
+#'
+angle_vectors <- function(x, y){
+  angle.d <- pracma::rad2deg(
+    acos(sum(x * y) / (sqrt(sum(x * x)) * sqrt(sum(y * y))))
+  )
+  return(angle.d)
+}
+
+#' @title Rotation matrix
+#'
+#' @description Calculates the rotation matrix using the rotation axis and the angle of rotation
+#' @author Tobias Stephan
+#' @param n Rotation axis (in cartesian coordinates). Can be a vector of three numbers
+#'  or a matrix of 3 columns (first one is x, second is y, thrid one is z)
+#' @param alpha Rotation angle in degrees
+#' @return matrix
+#' @export
+#' @examples
+#' w <- c(0, 1, 0)
+#' rotation_matrix(w, 90)
+#'
+rotation_matrix <-function(n, alpha){
+  n <- ppls::normalize.vector(n) # unit vector
+  R <- matrix(nrow=3, ncol=3)
+  R[1, 1] <- n[1]^2 * (1 - pracma::cosd(alpha)) + pracma::cosd(alpha)
+  R[1, 2] <- n[1] * n[2] * (1 - pracma::cosd(alpha)) - n[3] * pracma::sind(alpha)
+  R[1, 3] <- n[1] * n[3] * (1 - pracma::cosd(alpha)) + n[2] * pracma::sind(alpha)
+  R[2, 1] <- n[2] * n[1] * (1 - pracma::cosd(alpha)) + n[3] * pracma::sind(alpha)
+  R[2, 2] <- n[2]^2 * (1 - pracma::cosd(alpha)) + pracma::cosd(alpha)
+  R[2, 3] <- n[2] * n[3] * (1 - pracma::cosd(alpha)) - n[1] * pracma::sind(alpha)
+  R[3, 1] <- n[3] * n[1] * (1 - pracma::cosd(alpha)) - n[2] * pracma::sind(alpha)
+  R[3, 2] <- n[3] * n[2] * (1 - pracma::cosd(alpha)) + n[1] * pracma::sind(alpha)
+  R[3, 3] <- n[3]^2 * (1 - pracma::cosd(alpha)) + pracma::cosd(alpha)
+  return(R)
+}
+
+#' @title Longitude correction
+#'
+#' @description Corrects the longitude value to values between -180 and +180 degree
+#' @param longitude longitude(s) in degree
+#' @return number
+#' @export
+#' @examples
+#' longitude_modulo(-361)
+#'
+longitude_modulo <- function(longitude){
+  longitude.mod <- (longitude %% 360 + 540) %% 360 - 180
+  return(longitude.mod)
+}
+
+#' @title Cartesian to geographical coordinates
+#'
+#' @description Converts vector from cartesian into geographical coordinates
+#' @param n Vector of three numbers (x, y, z)
+#' @return vector of two numbers (latitude, longitude)
+#' @export
+#' @examples
+#' u <- c(1, -2, 3)
+#' cartesian_to_geographical(u)
+cartesian_to_geographical <- function(n) {
+  r <- sqrt(n[1]^2 + n[2]^2 + n[3]^2)
+  lat <- pracma::rad2deg(asin(n[3] / r))
+  lon <- pracma::rad2deg(atan2(n[2], n[1]))
+  if(lat <= -90){lat <- 180 + lat}
+  if(lat >=  90){lat <- 180 - lat}
+  lon <- longitude_modulo(lon)
+  return(c(lat, lon))
+}
+
+#' @title Geographical to cartesian coordinates
+#'
+#' @description Converts vector from geographical into cartesian  coordinates
+#' @param p vector of two numbers (latitude, longitude)
+#' @param r radius. default is 1.
+#' @return Vector of three numbers (x, y, z)
+#' @export
+#' @examples
+#' u <- c(50, 10)
+#' geographical_to_cartesian(u)
+geographical_to_cartesian <- function(p, r=1) {
+  if(missing(r)){
+    r <- 1
+  }
+  x <- c()
+  x[1] <- r * pracma::cosd(p[1]) * pracma::cosd(p[2])
+  x[2] <- r * pracma::cosd(p[1]) * pracma::sind(p[2])
+  x[3] <- r * pracma::sind(p[1])
+  return(x)
+}
+
+#' @title Absolute plate velocity
+#'
+#' @description Calculates the absolute angular velocity of plate motion
+#' @param w Angular velocity or rate or angle of rotation
+#' @param alpha Angular distance to Euler pole or smallcircle around Euler pole
+#' @param r radius. Default is Earth's radius (r=6371.00887714)
+#' @return number
+#' @export
+#' @examples
+#' abs_vel(0.21, 0)
+#' abs_vel(0.21, 45)
+#' abs_vel(0.21, 90)
+abs_vel <- function(w, alpha, r=6371.00887714) {
+  v <- pracma::deg2rad(w) * r * pracma::sind(alpha)
+  return(v)
+}
