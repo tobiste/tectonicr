59ba382b09f05473b9660c7fa54fa5900a6fb878
Tobias Stephan <tobias.stephan@ucalgary.ca>
2022-01-17 12:57:52 -0700

    added plate geometry data set

---
+++ R/PB2002.R
@@ -0,0 +1,18 @@
+#' Global set of present plate boundaries on the Earth (PB2002)
+#'
+#' Global set of present plate boundaries on the Earth (PB2002) by Bird 2003
+#'
+#' @docType data
+#'
+#' @usage data(PB2002)
+#'
+#' @format An object of class \code{""SpatialPolygonsDataFrame""}
+#'
+#' @references  Bird, P. (2003), An updated digital model of plate boundaries,
+#' Geochem. Geophys. Geosyst., 4, 1027, doi:10.1029/2001GC000252, 3.
+#' @keywords datasets
+#' @examples
+#'
+#' data(PB2002)
+#' head(PB2002)
+"PB2002"
